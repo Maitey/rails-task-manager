3266dba781a23a9f31cbba43aa3dc75ed28b0cd2
Maite Youyoutte <maite.youyoutte@hotmail.fr>
2026-04-10 00:43:54 +0200

    rails task manager

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+get    "tasks",          to: "tasks#index"
+get    "tasks/new",      to: "tasks#new"
+post   "tasks",          to: "tasks#create"
+get    "tasks/:id",      to: "tasks#show"
+get    "tasks/:id/edit", to: "tasks#edit"
+patch  "tasks/:id",      to: "tasks#update"
+delete "tasks/:id",      to: "tasks#destroy"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
